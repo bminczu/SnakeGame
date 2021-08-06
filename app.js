48e9c7f63a2fccc0735874d7e6fa8a148010dcb2
bminczu <bminczu@gmail.com>
2021-08-06 11:50:37 -0500

    declaring variables to keep track

---
+++ app.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const squares = document.querySelectorAll('.grid div')
+    const scoreDisplay = document.querySelector('span')
+    const startBtn = document.querySelector('start')
+
+    const width = 10
+    let currentIndex = 0 //first div in the grid
+    let appleIndex = 0 //first div in grid
+    let currentSnake = [2,1,0] //snake head is a 2, body pcs. 1 and 0 the end
+    let direction = 1
+    let score = 0
+    let speed = 0.9
+    let intervalTime = 0
+    let interval = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
